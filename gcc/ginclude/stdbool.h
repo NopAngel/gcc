05fdb8d713dcb614798539e5201c5787acfaa755
angxl      x86_64 <155915830+NopAngel@users.noreply.github.com>
2026-03-28 20:30:47 -0400

    `gcc/ginclude/stdbool.h: Native C23 Support`

--- gcc/ginclude/stdbool.h
+++ gcc/ginclude/stdbool.h
@@ -1,51 +1,34 @@
-/* Copyright (C) 1998-2026 Free Software Foundation, Inc.
-
-This file is part of GCC.
-
-GCC is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3, or (at your option)
-any later version.
-
-GCC is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-Under Section 7 of GPL version 3, you are granted additional
-permissions described in the GCC Runtime Library Exception, version
-3.1, as published by the Free Software Foundation.
-
-You should have received a copy of the GNU General Public License and
-a copy of the GCC Runtime Library Exception along with this program;
-see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
-<http://www.gnu.org/licenses/>.  */
-
-/*
- * ISO C Standard:  7.16  Boolean type and values  <stdbool.h>
- */
+/* ISO C Standard: 7.16 Boolean type and values <stdbool.h>
+   Optimized & Modernized for Fork Project. */
 
 #ifndef _STDBOOL_H
 #define _STDBOOL_H
 
 #ifndef __cplusplus
 
-#if defined __STDC_VERSION__ && __STDC_VERSION__ > 201710L
-/* bool, true and false are keywords.  */
+/* C23 (STDC 2023) and later versions already define bool, true, and false as keywords.
+ We check the value of __STDC_VERSION__ to avoid redefining them. */
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ > 201710L
 #else
-#define bool	_Bool
-#define true	1
-#define false	0
+  /* Compatible with C99, C11 and C17 */
+  #ifndef bool
+    #define bool  _Bool
+    #define true  1
+    #define false 0
+  #endif
 #endif
 
 #else /* __cplusplus */
 
-/* Supporting _Bool in C++ is a GCC extension.  */
-#define _Bool	bool
+/* In C++, bool is already native. 
+	We define _Bool for cross-compatibility with C code that uses GCC's internal type. */
+#ifndef _Bool
+  #define _Bool bool
+#endif
 
 #endif /* __cplusplus */
 
-/* Signal that all the definitions are present.  */
-#define __bool_true_false_are_defined	1
+/* Standard: Indicates that the definitions are present. */
+#define __bool_true_false_are_defined 1
 
-#endif	/* stdbool.h */
+#endif /* _STDBOOL_H */
